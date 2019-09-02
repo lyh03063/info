1b4d4b6cf7a9372dff65e9c4417ff1a92d6a850a
lyh <113381161@qq.com>
2019-09-02 09:05:17 +0800

    lyh

---
+++ src/main.js
@@ -0,0 +1,256 @@
+
+
+
+
+
+// import Vue from 'vue'
+Vue.config.productionTip = false
+
+import lodash from 'lodash'//导入lodash方法库
+window.lodash=lodash
+import axios from "axios";
+window.axios = axios;
+
+import ajax from "@/assets/js/ajax.js";
+window.ajax = ajax;
+
+
+
+
+
+import moment from "moment";
+window.moment = moment; 
+// import  "./assets/js/mix.js";//注意位置要提前
+
+import util from "@/assets/js/util.js";
+window.util = util; 
+
+
+
+
+
+
+
+// import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+import "./mock.js";
+
+
+import login from "@/login";
+import manage from "@/manage";
+import listHome from "@/page/listHome";
+import listCategory from "@/page/listCategory";
+import listCommodity from "@/page/listCommodity";
+
+
+
+import list_lyh from "@/page/list_lyh";
+import list_piece from "@/page/list_piece";
+
+import list_venue from "@/page/list_venue";
+import list_franchisee from "@/page/list_franchisee";
+import list_match from "@/page/list_match";
+import list_enroll from "@/page/list_enroll";
+import list_achievement from "@/page/list_achievement";
+import list_member from "@/page/list_member";
+import list_sponsor from "@/page/list_sponsor";
+import list_sponsorship from "@/page/list_sponsorship";
+import list_article_category from "@/page/list_article_category";
+import list_article from "@/page/list_article";
+import list_admin from "@/page/list_admin";
+import list_area from "@/page/list_area";
+import list_msg from "@/page/list_msg";
+import list_msg_read from "@/page/list_msg_read";
+import list_recommend from "@/page/list_recommend";
+import dynamic_form_demo from "@/page/dynamic_form_demo";
+import demo_common from "@/demo/common.vue";
+
+
+// window.util=util;
+
+// 3. 创建 router 实例，然后传 `routes` 配置
+const router = new VueRouter({
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: login },
+    {
+      path: '/manage',
+      component: manage,
+      redirect: '/listHome', //跳转
+      children: [//子路由
+        {
+          path: '/list_piece',
+          component: list_piece
+        },
+        {
+          path: '/list_venue',
+          component: list_venue
+        },
+        {
+          path: '/list_franchisee',
+          component: list_franchisee 
+        },
+        {
+          path: '/list_match',
+          component: list_match
+        },
+        {
+          path: '/list_enroll',
+          component: list_enroll
+        },
+        {
+          path: '/list_achievement',
+          component: list_achievement
+        },
+        {
+          path: '/list_member',
+          component: list_member
+        },
+        {
+          path: '/list_sponsor',
+          component: list_sponsor
+        },
+        {
+          path: '/list_sponsorship',
+          component: list_sponsorship
+        },
+        {
+          path: '/list_article_category',
+          component: list_article_category
+        },
+        {
+          path: '/list_article',
+          component: list_article
+        },
+        {
+          path: '/list_area',
+          component: list_area
+        },
+        {
+          path: '/list_admin',
+          component: list_admin
+        },
+        {
+          path: '/list_msg',
+          component: list_msg
+        },
+        {
+          path: '/list_msg_read',
+          component: list_msg_read
+        },
+        {
+          path: '/listHome',
+          component: listHome
+        },
+        {
+          path: '/listCommodity',
+          component: listCommodity
+        },
+        {
+          path: '/listCategory',
+          component: listCategory,
+        },
+        {
+          path: '/list_recommend',
+          component: list_recommend
+        },
+      
+       
+        
+        {
+          path: '/list_lyh',
+          component: list_lyh
+        },
+       
+        {
+          path: '/dynamic_form_demo',
+          component: dynamic_form_demo
+        },
+        {
+          path: '/demo_common',
+          component: demo_common
+        },
+        
+      ]
+    },
+  ]
+})
+
+
+
+
+// import Vuex from 'vuex'//导入vuex模块
+// Vue.use(Vuex)//应用组件
+
+const store = new Vuex.Store({//定义Vuex的存储对象
+  state: {
+    debug:false,
+    activeMenuIndex: "",//当前激活的菜单index
+    listState: {//存放列表的共享状态，
+
+    }, 
+    defultFindJson: {//存放列表的默认查询参数，
+      // list_article:{articleCategory:3  }
+
+    },   
+  },
+ 
+  mutations: {//变更事件
+    setDebug(state, param) {//设置debug模式
+      state.debug= param;
+      
+    },
+    setListFindJson(state, param) {//设置列表的初始筛选参数值
+      state.defultFindJson[param.listIndex] = param.findJson;
+      //对listState进行整个对象的变更（深拷贝），因为listState是有注册的，可以触发响应
+      let str = JSON.stringify(state.defultFindJson)//对象转换成字符串
+      state.defultFindJson = JSON.parse(str)//字符串转换成对象
+    },
+
+    initListState(state, param) {//改变列表的初始状态值
+      state.listState[param.listIndex] = param.objState;
+      //对listState进行整个对象的变更（深拷贝），因为listState是有注册的，可以触发响应
+      let str = JSON.stringify(state.listState)//对象转换成字符串
+      state.listState = JSON.parse(str)//字符串转换成对象
+    },
+    changeActiveMenu(state, activeMenuIndex) {//改变聚焦菜单
+      state.activeMenuIndex = activeMenuIndex
+    },
+    openDialogAdd(state, listIndex) {//打开新增弹窗事件
+      state.listState[listIndex].isShowDialogAdd = true;
+    },
+    closeDialogAdd(state, listIndex) {//关闭新增弹窗事件
+      state.listState[listIndex].isShowDialogAdd = false;
+    },
+    openDialogDetail(state, param) {//打开详情弹窗事件
+      state.listState[param.listIndex].isShowDialogDetail = true;
+      // state.listState[param.listIndex].tableDataDetail.forEach(doc => {
+      //   //遍历详情弹窗的表格数据
+      //   doc.itemValue = param.row[doc.field]; //修改itemValue
+      // });
+      state.listState[param.listIndex].row = param.row;//将行数据保存到vuex
+    },
+    closeDialogDetail(state, listIndex) {//关闭详情弹窗事件
+      state.listState[listIndex].isShowDialogDetail = false;
+    },
+  }
+})
+
+Vue.prototype.$store = store//让vue实例中可访问$store
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+
+// Vue.use(ElementUI);
+
+
+// import dm_components from "dmagic-components";
+// Vue.use(dm_components);
+
+
+
+
+import Main from './main.vue'
+new Vue({
+  render: h => h(Main),
+  router,
+}).$mount('#app')
---
+++ src/page/list_lyh.vue
@@ -0,0 +1,241 @@
+
+<template>
+  <div class>
+    <upload_img v-model="arr1" ></upload_img>
+    <dm_debug_list>
+      <dm_debug_item v-model="cfForm" text="cfForm" />
+    </dm_debug_list>
+
+    <dm_dynamic_form :cf="cfForm" v-model="formData"></dm_dynamic_form>
+  </div>
+</template>
+<script>
+import upload_img from "@/components/form_item/upload_img.vue";
+
+export default {
+  components: {upload_img},
+
+  data() {
+    return {
+      arr1:[],
+      formData: {
+        extend: { aaa: 1, name: "lucy" },
+        prop_checkbox: [], //复选框字段的默认数组
+        prop1: "abcd",
+        prop_dateTime: "2019-7-24 14:09",
+        diycheckbox: [1]
+      },
+      cfForm: {
+        labelWidth: "150px",
+        formItems: [
+          //   {
+          //   label: "extend",
+          //   prop: "extend",
+          //   type: "jsonEditor",
+
+          // },
+          {
+            label: "联系人信息",
+            prop: "extend",
+            default: {},
+            cfForm: {
+              formItems: [
+                {
+                  label: "姓名",
+                  prop: "name",
+                  type: "input"
+                },
+                {
+                  label: "下拉框(select)",
+                  prop: "sex",
+                  type: "select",
+                  default: 2,
+                  options: [
+                    { value: 1, label: "男" },
+                    { value: 2, label: "女" }
+                  ]
+                },
+                {
+                  label: "联系人信息2",
+                  prop: "extend1",
+                  default: {},
+
+                  cfForm: {
+                    col_span: 12,
+                    formItems: [
+                      {
+                        label: "姓名",
+                        prop: "name",
+                        type: "input",
+                        col_span: 12
+                      },
+                      {
+                        label: "下拉框(select)",
+                        prop: "sex",
+                        type: "select",
+                        default: 2,
+                        options: [
+                          { value: 1, label: "男" },
+                          { value: 2, label: "女" }
+                        ],
+                        col_span: 12
+                      }
+                    ]
+                  }
+                }
+              ]
+            }
+          },
+          {
+            label: "负责人信息",
+            // prop: "extend",
+            cfForm: {
+              formItems: [
+                {
+                  label: "姓名",
+                  prop: "name",
+                  type: "input"
+                },
+                {
+                  label: "下拉框(select)",
+                  prop: "sex",
+                  type: "select",
+                  default: 2,
+                  options: [
+                    { value: 1, label: "男" },
+                    { value: 2, label: "女" }
+                  ]
+                }
+              ]
+            }
+          },
+          {
+            label: "纬度",
+            prop: "extend",
+            path: "latitude"
+          },
+          {
+            label: "普通文本框(input)",
+            prop: "prop1",
+            type: "input",
+            default: "默认文本",
+            rules: [
+              { required: true, message: "不能为空" },
+
+              {
+                pattern: /^[\u4E00-\u9FA5]+$/,
+                message: "用户名只能为中文"
+              }
+            ]
+          },
+          {
+            label: "密码框2(password)",
+            prop: "prop_password",
+            type: "password"
+          },
+          {
+            label: "用于模糊查询文本框(input_find_vague)",
+            prop: "prop_input_find_vague",
+            type: "input_find_vague"
+          },
+          {
+            label: "文本域(textarea)",
+            prop: "prop_textarea",
+            type: "textarea"
+          },
+          {
+            label: "下拉框(select)",
+            prop: "sex",
+            type: "select",
+            default: 2,
+            options: [{ value: 1, label: "男" }, { value: 2, label: "女" }]
+          },
+          {
+            label: "sex【联动】",
+            prop: "sex_relative",
+            type: "input",
+            //显示条件
+            term: { $or: [{ sex: 2 }, { prop_textarea: 2 }] }
+          },
+          {
+            label: "下拉框(select+ajax)",
+            prop: "prop4",
+            type: "select",
+            ajax: {
+              url: "/crossList?page=mabang-member",
+              param: { a: 1 },
+              keyLabel: "nickName",
+              keyValue: "userName"
+            }
+          },
+
+          {
+            label: "单选框(radio)",
+            prop: "prop_radio",
+            type: "radio",
+            default: 2,
+            options: [{ value: 1, label: "男" }, { value: 2, label: "女" }]
+          },
+          {
+            label: "复选框(checkbox)",
+            prop: "prop_checkbox",
+            type: "checkbox",
+            default: [2],
+            options: [{ value: 1, label: "男" }, { value: 2, label: "女" }]
+          },
+          {
+            label: "日期时间(dateTime)",
+            prop: "prop_dateTime",
+            type: "dateTime"
+          },
+          {
+            label: "日期选择器(date)",
+            prop: "prop_date",
+            type: "date"
+          },
+          {
+            label: "图片上传",
+            prop: "prop_upload",
+            type: "upload"
+          },
+          {
+            label: "json编辑器(jsonEditor)",
+            prop: "prop_jsonEditor",
+            type: "jsonEditor"
+          },
+          {
+            label: "json编辑器(vueJsonEditor)",
+            prop: "prop_vueJsonEditor",
+            type: "vueJsonEditor"
+          },
+          {
+            label: "富文本编辑器(editor)",
+            prop: "prop_editor",
+            type: "editor"
+          },
+          {
+            label: "自定义组件(slot实现)",
+            prop: "description",
+            slot: "slot_form_item_description",
+            rules: [{ required: true, message: "不能为空" }]
+          },
+          {
+            label: "diycheckbox(slot实现)",
+            prop: "diycheckbox",
+            slot: "slot_form_item_diycheckbox",
+            rules: [{ required: true, message: "不能为空" }]
+          }
+        ],
+        btns: [
+          { text: "提交111", event: "submit", type: "primary", validate: true },
+          { text: "取消222", event: "cancel" }
+        ]
+      }
+    };
+  },
+
+  async created() {},
+
+  methods: {}
+};
+</script>
